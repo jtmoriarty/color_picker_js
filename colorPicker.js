a47d4b266850a37a34cfd061dca37d820f36ae3f
Jon <jon@jon-desktop.(none)>
2013-03-21 21:38:36 -0400

    Pannel shows more colors with less duplicates

--- colorPicker.js
+++ colorPicker.js
@@ -1,63 +1,49 @@
 (function( $ ) {
 $.fn.colorPicker = function() {
 	return this.each(function() {
-
-
 		$(this).click(function(){
-			var r=0;
-			var g=0;
-			var b=0;
-
 			$('#color-picker-pannel').toggle();
+			
 			if($('.color-btn').is('*')){
 
 			}else{
-			for(r=0; r < 17; r++){
-				for(g=0; g < 17; g++){
-					for(b=0; b < 17; b++){
-						if(r>9 || g>9 || b>9){
-							r=r.toString(16);
-							g=g.toString(16);
-							b=b.toString(16);
+				for(var r=0; r <= 16; r++){
+					for(var g=0; g <= 16; g++){
+						for(var b=0; b <= 16; b++){
+							var subString = r.toString(16) + g.toString(16) + b.toString(16);
+							if(subString.length < 4){
+								$('#color-row').append("<span class=color-btn id='color-btn" + r.toString(16) + g.toString(16) + b.toString(16) + "' style='background-color: #" + r.toString(16) + g.toString(16) + b.toString(16) + "'></span>");
+							}else{
+
+							}
 						}
-						$('#color-row').append("<span class=color-btn id='color-btn" + r + g + b + "' style='background-color: #" + r + g + b + "'></span>");
 					}
 				}
 			}
-		}
-		
-			$('.color-btn').click(function(){
-				var color_id = $(this).attr('id');
-				var color_hex_value = $('#' + color_id).css('background-color');
-				var css_value = $('#page_style_css').attr('value');
-				$('#page_style_css').attr('value', css_value + 'color: ' + color_hex_value + ';');
-			});
-		});
 
-			$('#font_color_pannel').toggle();
-			$('.color_picker_btn').click(function(){
+			$('.color-btn').mouseenter(function(){
 				var color_id = $(this).attr('id');
-				var color_hex_value = $('#' + color_id).css('background-color');
-				var css_value = $('#page_style_css').attr('value');
-				$('#page_style_css').attr('value', css_value + 'color: ' + color_hex_value + ';');
-			});
-
-			$('.color_picker_btn').mouseenter(function(){
-				var color_id = $(this).attr('id');
-				$('#' + color_id).css('margin-top', '-40px');
+				$('#' + color_id).css('margin-top', '-30px');
 				$('#' + color_id).css('margin-left', '-30px');
-				$('#' + color_id).css('height', '40px');
-				$('#' + color_id).css('width', '40px');
+				$('#' + color_id).css('height', '30px');
+				$('#' + color_id).css('width', '30px');
 				var color_hex_value = $('#' + color_id).css('background-color');
-				$('#color_hex_display').text(color_hex_value);
+				$('#color-hex-display').text(color_hex_value);
 			});
 
-			$('.color_picker_btn').mouseleave(function(){
+			$('.color-btn').mouseleave(function(){
 				var color_id = $(this).attr('id');
 				$('#' + color_id).css('margin-top', '0px');
 				$('#' + color_id).css('margin-left', '0px');
 				$('#' + color_id).css('height', '10px');
 				$('#' + color_id).css('width', '10px');
+			});
+		
+			$('.color-btn').click(function(){
+				var color_id = $(this).attr('id');
+				var color_hex_value = $('#' + color_id).css('background-color');
+				$('#color-input').attr('value', 'color: ' + color_hex_value + ';');
+			});
 			});
 		});
 	};
---
+++ colorPickerElements.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$('body').html("<input type='text' id='color-input' />");
+	$('body').append("<div id='color-picker-pannel'></div>");
+	$("#color-picker-pannel").html("<div id='color-row'></div>");
+});
